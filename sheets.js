a8225214a58dc1e751eb3ee81b0975c3980ef02b
Marisol Yelamo <marisolrocioyelamo@gmail.com>
2025-10-05 21:07:36 -0300

    require node -fech

--- sheets.js
+++ sheets.js
@@ -1,7 +1,7 @@
 const { google } = require("googleapis");
 const fs = require("fs");
 const path = require("path");
-import fetch from "node-fetch";
+const fetch = requiere("node-fetch");
 
 const SCOPES = ["https://www.googleapis.com/auth/spreadsheets"];
 
