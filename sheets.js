4b2d8804620dfea02fa4615d7aea237184d29e53
Marisol Yelamo <marisolrocioyelamo@gmail.com>
2025-10-05 21:01:45 -0300

    add node fech depemdencie

--- sheets.js
+++ sheets.js
@@ -1,6 +1,7 @@
 const { google } = require("googleapis");
 const fs = require("fs");
 const path = require("path");
+import fetch from "node-fetch";
 
 const SCOPES = ["https://www.googleapis.com/auth/spreadsheets"];
 
